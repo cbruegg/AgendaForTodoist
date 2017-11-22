b5ad112829753817216c93bde74e8b98d92d9365
Christian Brüggemann <mail@cbruegg.com>
2017-11-22 22:57:37 +0100

    MainActivity only as relay

--- wear/src/main/java/com/cbruegg/agendafortodoist/MainActivity.kt
+++ wear/src/main/java/com/cbruegg/agendafortodoist/MainActivity.kt
@@ -2,18 +2,13 @@ package com.cbruegg.agendafortodoist
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
 import com.cbruegg.agendafortodoist.projects.ProjectsActivity
 
 class MainActivity : WearableActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Enables Always-on
-        setAmbientEnabled()
-
         Intent(this, ProjectsActivity::class.java).let { startActivity(it) }
+        finish()
     }
 }
